dfaf02d79d53d0892e0b0d6d588cfcfb045c8b8f
illuminateee <ariqakbar.muhammad.ma16@gmail.com>
2023-09-27 18:52:05 +0700

    A simple web using HTML,CSS and JS

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+function scrollToHome(){
+    let targetElement = document.getElementById('home-content');
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+}
+function scrollToTes(){
+    let targetElement = document.getElementById('testimonial-content');
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+}
+function scrollToAbout(){
+    let targetElement = document.getElementById('about-content');
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+}
